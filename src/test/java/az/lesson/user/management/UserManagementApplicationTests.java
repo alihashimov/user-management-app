d87ffb5c8449ff8ff69f9d59d127e99421eb3714
Ali Hashimov <ali.hashimovv@gmail.com>
2022-12-19 19:39:04 +0400

    initial commit

---
+++ src/test/java/az/lesson/user/management/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package az.lesson.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
